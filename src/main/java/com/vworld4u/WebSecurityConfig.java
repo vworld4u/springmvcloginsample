bd4e2e239fd8c3d742ca4d42f4580520b7ea03e0
Venkatesha T R <vworld4u@gmail.com>
2016-11-20 17:09:10 +0530

    feat: Logout added

--- src/main/java/com/vworld4u/WebSecurityConfig.java
+++ src/main/java/com/vworld4u/WebSecurityConfig.java
@@ -15,11 +15,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/register", "/hello", "/login").permitAll()
-        .anyRequest().authenticated() 
-        .and().logout().logoutUrl("/logout").invalidateHttpSession(true).logoutSuccessUrl("/login")
+        http
+//        .logout().logoutUrl("/logout").invalidateHttpSession(true).logoutSuccessUrl("/login?logout").and()
+        .authorizeRequests().antMatchers("/", "/register", "/hello", "/login", "/logout").permitAll()
+        .anyRequest().authenticated()
         .and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/home");
-        
     }
 
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/vworld4u/controllers/ApplicationController.java
+++ src/main/java/com/vworld4u/controllers/ApplicationController.java
@@ -2,9 +2,15 @@ package com.vworld4u.controllers;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -92,4 +98,14 @@ public class ApplicationController {
 		}
 		return "register";
 	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){    
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good idea to show login screen again.
+	}
+
 }
